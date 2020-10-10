c23cc58583991225df3f4ae6a4962754d5826ca1
Peter Thurner | Blunix GmbH <p.thurner@blunix.com>
2020-10-10 04:40:37 +0000

    update role for debian 11

--- molecule/default/tests/test_presense.py
+++ molecule/default/tests/test_presense.py
@@ -5,5 +5,10 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_executable_exists(host):
     assert host.file('/sbin/mount.nfs4').exists
+
+
+def test_testfile_in_mountpoint_exists(host):
+    assert host.file('/srv/testfile.txt').exists
